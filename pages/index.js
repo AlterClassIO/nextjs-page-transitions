efd7b6a32ba4b10fcf004b21683b8c4e874fb7d7
Grégory D'Angelo <dangelo.gregory@gmail.com>
2022-03-21 15:46:19 +0100

    feat: Add movie info on cards

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
+import { format } from 'date-fns';
 import movieDb from '@/lib/movieDb';
 import Layout from '@/components/Layout';
 
@@ -47,21 +48,40 @@ export default function Home({ movies = [] }) {
             className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-8"
           >
             {movies.map(movie => (
-              <Link href={`/${movie.id}`} key={movie.id} passHref>
-                <motion.a
-                  className="relative aspect-w-9 aspect-h-14 rounded-lg overflow-hidden cursor-pointer bg-gray-100"
-                  key={movie.id}
-                  whileHover={{ scale: 1.05, opacity: 0.8, zIndex: 1 }}
-                  whileTap={{ scale: 0.95 }}
-                  variants={fadeInUp}
-                >
-                  <Image
-                    src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-                    alt={movie.title}
-                    layout="fill"
-                  />
-                </motion.a>
-              </Link>
+              <motion.div
+                key={movie.id}
+                whileHover={{ scale: 1.05, zIndex: 1 }}
+                whileTap={{ scale: 0.95 }}
+                variants={fadeInUp}
+              >
+                <Link href={`/${movie.id}`}>
+                  <a className="relative block">
+                    <div
+                      className="relative block aspect-w-9 aspect-h-14 rounded-lg overflow-hidden cursor-pointer bg-gray-100"
+                      key={movie.id}
+                    >
+                      <Image
+                        src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                        alt={movie.title}
+                        layout="fill"
+                      />
+                    </div>
+
+                    <div className="absolute bottom-0 left-2 transform translate-y-1/2 bg-gray-900 border text-white w-10 h-10 rounded-full text-sm flex items-center justify-center font-semibold shadow-xl">
+                      {movie.vote_average}
+                    </div>
+                  </a>
+                </Link>
+
+                <div className="mt-6 px-2">
+                  <p className="font-semibold">{movie.title}</p>
+                  {movie?.release_date ? (
+                    <p className="text-gray-500">
+                      {format(new Date(movie.release_date), 'MMM dd, yyyy')}
+                    </p>
+                  ) : null}
+                </div>
+              </motion.div>
             ))}
           </motion.div>
         </div>
