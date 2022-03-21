117c83fee45cabacff2f60576e362b0f643170bf
Grégory D'Angelo <dangelo.gregory@gmail.com>
2022-03-21 14:49:07 +0100

    feat: Add animations + bug fixes

--- pages/[id].js
+++ pages/[id].js
@@ -1,88 +1,173 @@
+import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { format } from 'date-fns';
 import movieDb from '@/lib/movieDb';
+import { ArrowSmLeftIcon } from '@heroicons/react/outline';
+
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: 'easeOut',
+    },
+  },
+};
 
 const Movie = ({ movie = null, credits = null }) => {
   return (
-    <div>
-      <div className="space-y-10">
-        <div className="relative w-full h-full">
-          <div className="absolute z-10 inset-0 bg-gray-900 bg-opacity-90" />
+    <>
+      <Head>
+        <title>{movie?.title}</title>
+        <meta name="description" content={movie?.overview} />
+      </Head>
 
-          <Image
-            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-            layout="fill"
-          />
+      <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
+        <div className="space-y-10">
+          <div className="relative w-full h-full">
+            <div className="absolute z-10 inset-0 bg-gray-900 bg-opacity-90" />
 
-          <div className="container mx-auto text-white">
-            <div className="flex gap-12 px-4 sm:px-6 py-12">
-              <div className="z-10 shrink-0">
-                {movie?.poster_path ? (
-                  <Image
-                    src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-                    width={300}
-                    height={450}
-                    className="rounded-lg"
-                  />
-                ) : null}
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              className="absolute inset-0"
+            >
+              {movie?.backdrop_path ? (
+                <Image
+                  src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                  alt=""
+                  layout="fill"
+                />
+              ) : null}
+            </motion.div>
+
+            <div className="container mx-auto text-white">
+              <div className="flex gap-12 px-4 sm:px-6 py-12">
+                <motion.div
+                  variants={{
+                    initial: { x: 100, opacity: 0 },
+                    animate: { x: 0, opacity: 1 },
+                  }}
+                  className="z-10 shrink-0"
+                >
+                  {movie?.poster_path ? (
+                    <Image
+                      src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                      alt="Poster"
+                      width={300}
+                      height={450}
+                      className="rounded-lg"
+                    />
+                  ) : null}
+                </motion.div>
+
+                <motion.div
+                  variants={{
+                    animate: {
+                      transition: { staggerChildren: 0.05 },
+                    },
+                  }}
+                  className="relative z-10 py-6"
+                >
+                  <Link href="/" passHref>
+                    <motion.a
+                      variants={fadeInUp}
+                      className="text-sm text-opacity-50 hover:text-opacity-100 inline-flex items-center space-x-1 transition"
+                    >
+                      <ArrowSmLeftIcon className="shrink-0 w-5 h-5 mt-px" />
+                      <span>Back to movies</span>
+                    </motion.a>
+                  </Link>
+
+                  <motion.h1
+                    variants={fadeInUp}
+                    className="mt-4 font-bold text-4xl"
+                  >
+                    {movie?.title}
+                  </motion.h1>
+
+                  <motion.div
+                    variants={fadeInUp}
+                    className="mt-1 inline-flex items-center space-x-2 text-gray-400"
+                  >
+                    <span>
+                      {movie?.release_date
+                        ? format(new Date(movie.release_date), 'MM-dd-yyyy')
+                        : null}
+                    </span>
+                    <span>-</span>
+                    <span>
+                      {movie?.genres.map(({ name }) => name).join(', ')}
+                    </span>
+                  </motion.div>
+
+                  <motion.p
+                    variants={fadeInUp}
+                    className="mt-4 italic text-gray-300"
+                  >
+                    {movie?.tagline}
+                  </motion.p>
+
+                  <motion.div variants={fadeInUp} className="mt-6">
+                    <h3 className="font-semibold text-lg">Overview</h3>
+                    <p className="mt-1 text-gray-300">{movie?.overview}</p>
+                  </motion.div>
+                </motion.div>
               </div>
+            </div>
+          </div>
 
-              <div className="relative z-10 py-6">
-                <h1 className="font-bold text-4xl">{movie?.title}</h1>
+          <div className="container mx-auto">
+            <div className="px-4 sm:px-6 pb-12">
+              <h2 className="font-medium text-xl capitalize">
+                Top billed cast
+              </h2>
 
-                <div className="mt-1 inline-flex items-center space-x-2 text-gray-400">
-                  <span>
-                    {movie?.release_date
-                      ? format(new Date(movie.release_date), 'MM-dd-yyyy')
-                      : null}
-                  </span>
-                  <span>-</span>
-                  <span>
-                    {movie?.genres.map(({ name }) => name).join(', ')}
-                  </span>
-                </div>
-
-                <p className="mt-4 italic text-gray-300">{movie?.tagline}</p>
-
-                <div className="mt-6">
-                  <h3 className="font-semibold text-lg">Overview</h3>
-                  <p className="mt-1">{movie?.overview}</p>
-                </div>
+              <div className="overflow-y-auto">
+                <motion.ol
+                  variants={{
+                    animate: {
+                      transition: { staggerChildren: 0.1 },
+                    },
+                  }}
+                  className="mt-6 flex flex-row gap-4 pb-6"
+                >
+                  {credits?.cast?.slice(0, 10)?.map(actor => (
+                    <motion.li
+                      key={actor.id}
+                      variants={fadeInUp}
+                      whileHover={{ translateY: -10 }}
+                      className="shrink-0 rounded-lg w-[138px] overflow-hidden border border-opacity-50 shadow-md"
+                    >
+                      <div className="relative w-[138px] h-[175px] bg-gray-100">
+                        {actor?.profile_path ? (
+                          <Image
+                            src={`https://image.tmdb.org/t/p/original/${actor.profile_path}`}
+                            alt={actor.name}
+                            layout="fill"
+                          />
+                        ) : null}
+                      </div>
+                      <div className="p-4">
+                        <p className="truncate font-semibold">{actor.name}</p>
+                        <p className="truncate">{actor.character}</p>
+                      </div>
+                    </motion.li>
+                  ))}
+                </motion.ol>
               </div>
             </div>
           </div>
         </div>
-
-        <div className="container mx-auto">
-          <div className="px-4 sm:px-6 pb-12">
-            <h2 className="font-medium text-xl capitalize">Top billed cast</h2>
-
-            <ol className="mt-4 flex flex-row gap-4 overflow-y-auto pb-6">
-              {credits?.cast?.slice(0, 10)?.map(actor => (
-                <li
-                  key={actor.id}
-                  className="shrink-0 rounded-lg w-[138px] overflow-hidden border border-opacity-50 shadow-md"
-                >
-                  <div className="relative w-[138px] h-[175px] bg-gray-100">
-                    {actor?.profile_path ? (
-                      <Image
-                        src={`https://image.tmdb.org/t/p/original/${actor.profile_path}`}
-                        layout="fill"
-                      />
-                    ) : null}
-                  </div>
-                  <div className="p-4">
-                    <p className="truncate font-semibold">{actor.name}</p>
-                    <p className="truncate">{actor.character}</p>
-                  </div>
-                </li>
-              ))}
-            </ol>
-          </div>
-        </div>
-      </div>
-    </div>
+      </motion.div>
+    </>
   );
 };
 
@@ -125,7 +210,7 @@ export async function getStaticProps({ params }) {
     };
   } catch (e) {
     return {
-      props: null,
+      props: {},
     };
   }
 }
